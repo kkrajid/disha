a9a2bc14f0a9156cec29f54b1dabc9e8062cba25
kkrajid <kkrajid@gmail.com>
2025-03-30 02:24:01 +0530

    dashbaord updated r

--- src/pages/ProfileSetup.tsx
+++ src/pages/ProfileSetup.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 // Industry options
